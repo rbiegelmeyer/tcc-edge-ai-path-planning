ae5983075e5e1708c140bbb39961d2dc8a0ef2aa
rbiegelmeyer <roberto@shirou.com.br>
2025-10-05 20:19:48 -0300

    [UPD] Alterada forma de criação dos walls

--- AStar/main.c
+++ AStar/main.c
@@ -5,22 +5,33 @@
 #include <math.h>
 #include <string.h>
 
-#define WIDTH 25   
-#define HEIGHT 25
-#define DIFFICULTY 8
+#define WIDTH 20
+#define HEIGHT 20
+#define DIFFICULTY 1
 
 void printArray(int16_t array[HEIGHT][WIDTH])
 {
+    for (int16_t x = 0; x < (WIDTH + 2); x++)
+    {
+        printf("X");
+    }
+    printf("\n");
     for (int16_t y = 0; y < HEIGHT; y++)
     {
+        printf("X");
         for (int16_t x = 0; x < WIDTH; x++)
         {
             printf("%c", array[y][x] == 1   ? 'X'
                          : array[y][x] == 2 ? '.'
                                             : ' ');
         }
-        printf("\n");
+        printf("X\n");
     }
+    for (int16_t x = 0; x < (WIDTH + 2); x++)
+    {
+        printf("X");
+    }
+    printf("\n");
 }
 void printArrayNum(int16_t array[HEIGHT][WIDTH])
 {
@@ -59,7 +70,7 @@ void printCostArray(float array[HEIGHT][WIDTH])
     }
 }
 
-void insertObstacles(int16_t * array, int16_t height, int16_t width, int16_t difficulty)
+void insertObstacles(int16_t *array, int16_t height, int16_t width, int16_t difficulty)
 {
     // Set random seed
     // srand(time(0));
@@ -75,24 +86,8 @@ void insertObstacles(int16_t * array, int16_t height, int16_t width, int16_t dif
                 value = 0;
             // array[y][x] = value;
 
-            *(array + y * width + x) = value;  
+            *(array + y * width + x) = value;
         }
-    }
-
-    // Add walls
-    for (int16_t x = 0; x < width; x++)
-    {
-        // array[0][x] = 1;
-        // array[height - 1][x] = 1;
-        *(array + width * 0 + x) = 1;
-        *(array + width * (height - 1) + x) = 1;
-    }
-    for (int16_t y = 0; y < height; y++)
-    {
-        // array[i][0] = 1;
-        // array[i][width - 1] = 1;
-        *(array + width * y + 0) = 1;
-        *(array + width * y + (width - 1)) = 1;
     }
 }
 
@@ -132,7 +127,6 @@ void reconstruct(int16_t *current, int16_t parent[HEIGHT][WIDTH][2], int16_t arr
     int16_t temp_y = 0, temp_x = 0;
     while (*current != -1)
     {
-        // printf("1 - (%d,%d) <- (%d,%d)\n", current[1], current[0], parent[current[0]][current[1]][1], parent[current[0]][current[1]][0]);
         array[current[0]][current[1]] = 2;
 
         temp_x = parent[current[0]][current[1]][1];
@@ -149,13 +143,9 @@ void reconstruct(int16_t *current, int16_t parent[HEIGHT][WIDTH][2], int16_t arr
 
 int16_t steps[4][2] = {
     {0, 1},  // Direita
-    // {1, 1},  // Baixo Direita
     {1, 0},  // Baixo
-    // {1, -1}, // Baixo Esquerda
     {0, -1}, // Esquerda
-    // {-1, -1},// Cima Esquerda
     {-1, 0}, // Cima
-    // {-1, 1}  // Cima Direita
 };
 
 int main(int argc, char **argv)
@@ -179,16 +169,15 @@ int main(int argc, char **argv)
 
     // Defini posição de inicio e final
     // Levar em consideração o wall
-    int16_t start[2] = {1, 1};
-    int16_t end[2] = {HEIGHT - 2, WIDTH - 2};
+    int16_t start[2] = {0, 0};
+    int16_t end[2] = {HEIGHT - 1, WIDTH - 1};
     // int16_t end[2] = {1, 1};
     // int16_t start[2] = {HEIGHT - 2, WIDTH - 2};
-    
+
     array[start[0]][start[1]] = 2;
     array[end[0]][end[1]] = 2;
 
     insertObstacles((int16_t *)array, HEIGHT, WIDTH, DIFFICULTY);
-    printArray(array);
     calculateHeuristics(h, g, f, end, HEIGHT, WIDTH);
 
     open[start[0]][start[1]] = 1;
@@ -197,7 +186,6 @@ int main(int argc, char **argv)
     while (numOpen > 0 && done == 0)
     {
         current = getNewCurrent(f, array, open);
-        // printf("c (%d,%d)\n", current[1], current[0]);
 
         if (current[0] == end[0] && current[1] == end[1])
         {
@@ -213,14 +201,12 @@ int main(int argc, char **argv)
         // Itera ao redor do current
         int16_t x = 0;
         int16_t y = 0;
-        
         for (uint8_t step = 0; step < 4; step++)
         {
             x = current[1] + steps[step][1];
             y = current[0] + steps[step][0];
 
             if (x < 0 || y < 0 || x > WIDTH - 1 || y > HEIGHT - 1 || // Se for parede
-                // abs(current[0] - y) == abs(current[1] - x) ||        // Se for diagonal
                 closed[y][x] == 1 ||                                 // Se a rota está fechada/já foi usada/calculada os vizinhos
                 array[y][x] == 1)                                    // Se houver um obstaculo
             {
@@ -238,9 +224,9 @@ int main(int argc, char **argv)
             // }
             // tempG += 1.0f;
 
-            if (open[y][x] != 1)                         // Se já foi calculado
+            if (open[y][x] != 1) // Se já foi calculado
             {
-                open[y][x] = 1;  // Defini como calculado
+                open[y][x] = 1; // Defini como calculado
                 numOpen += 1;
             }
             g[y][x] = g[current[0]][current[1]] + 1.0f; // Atribui g
@@ -248,16 +234,13 @@ int main(int argc, char **argv)
 
             parent[y][x][0] = current[0];
             parent[y][x][1] = current[1];
-            // printf("  (%d,%d) %6.3f\n", x, y, f[y][x]);
         }
-        // printf("\n");
     }
     if (numOpen == 0 && done == 0)
     {
         done = 1;
         printf("\n");
         printArray(array);
-        // printCostArray(g);
         printf("\nNo solution!\n\n");
     }
 
